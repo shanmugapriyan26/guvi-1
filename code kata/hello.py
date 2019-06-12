9f8afc6ea4b19128a9b7a3236fa650616b1de04e
PAULESAKKI <51704403+PAULESAKKI@users.noreply.github.com>
2019-06-12 11:54:22 +0530

    Update hello.py

--- code kata/hello.py
+++ code kata/hello.py
@@ -1,7 +1,8 @@
 a=int(input())
 if(a>0):
-  print("Positive")
-elif(a<0):
-  print("Negative")
+  print("positive")
+elif((a<0):
+     print("Negative")
 else:
-  print("Zero")
+     print("zero")
+     
